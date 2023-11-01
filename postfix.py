8489011ddf7e2b2b2c0e647688e69eb4ea52babc
soumya <soumyakn89@gmail.com>
2023-11-01 16:49:31 +0530

    green:Test passed for addtion of two operand

--- postfix.py
+++ postfix.py
@@ -2,7 +2,11 @@ def evaluate(exp):
     stack=[]
     for i in exp:
         if i in ['+']:
-            pass
+            op2=stack.pop()
+            op1=stack.pop()
+            if i=='+':
+                stack.append(op2+op1)
+            
         else:
             stack.append(int(i))
     return stack.pop()
