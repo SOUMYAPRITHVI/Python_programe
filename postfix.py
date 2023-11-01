df0c866cbe0803e3680211731094d5eba2463bde
soumya <soumyakn89@gmail.com>
2023-11-01 16:35:10 +0530

    Green:Test passed for single operand evaluation

--- postfix.py
+++ postfix.py
@@ -1,3 +1,8 @@
 def evaluate(exp):
     stack=[]
-    return exp
+    for i in exp:
+        if i in ['+']:
+            pass
+        else:
+            stack.append(int(i))
+    return stack.pop()
