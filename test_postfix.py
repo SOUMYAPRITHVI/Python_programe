2ba79e50189f192bdc0fe4e00773eda591dde7ac
soumya <soumyakn89@gmail.com>
2023-11-01 17:41:14 +0530

    Red:More than one operation

--- test_postfix.py
+++ test_postfix.py
@@ -2,7 +2,6 @@ from postfix import evaluate
 
 def test_single_operand():
     assert evaluate("5")==5
-
 def test_add():
     assert evaluate("52+")==7
 def test_minus():
@@ -13,3 +12,5 @@ def test_mul():
     assert evaluate("25*")==10
 def test_div():
     assert evaluate("52/")==2
+def test_exp():
+    assert evaluate("542/*6+")==18
