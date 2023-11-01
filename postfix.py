6f64d2cd8d89111273edf24800c2b104d5facce3
soumya <soumyakn89@gmail.com>
2023-11-01 17:27:39 +0530

    Green:Division of two operand

--- postfix.py
+++ postfix.py
@@ -1,7 +1,7 @@
 def evaluate(exp):
     stack=[]
     for i in exp:
-        if i in ['+','-','*']:
+        if i in ['+','-','*','/']:
             op2=stack.pop()
             op1=stack.pop()
             if i=='+':
@@ -10,7 +10,8 @@ def evaluate(exp):
                 stack.append(op1-op2)
             elif i=='*':
                 stack.append(op1*op2)
-            
+            elif i=='/':
+                stack.append(op1//op2)
         else:
             stack.append(int(i))
     return stack.pop()
