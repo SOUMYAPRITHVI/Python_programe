3d1013a905f3c9fab5757c4ebcd11b24b9efe795
soumya <soumyakn89@gmail.com>
2023-11-01 17:17:04 +0530

    Green:Multiplication of two operand

--- postfix.py
+++ postfix.py
@@ -1,13 +1,15 @@
 def evaluate(exp):
     stack=[]
     for i in exp:
-        if i in ['+','-']:
+        if i in ['+','-','*']:
             op2=stack.pop()
             op1=stack.pop()
             if i=='+':
                 stack.append(op1+op2)
             elif i=='-':
                 stack.append(op1-op2)
+            elif i=='*':
+                stack.append(op1*op2)
             
         else:
             stack.append(int(i))
